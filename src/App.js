59ae89e2581e38db9c8793dceaaeb6b254533391
SHASHANK SHEKHAR <shashank.shekhar0331@gmail.com>
2024-06-17 21:33:50 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return <div>
+    <h1>Hello {value}</h1>
+    <h2>This is my React static Web Application</h2>
+    <p>My Name is Shashank Shekhar</p>
+    </div>;
 }
 
 export default App;
